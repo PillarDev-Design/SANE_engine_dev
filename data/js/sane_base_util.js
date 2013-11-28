c125c3151f0b87ba0c5422796e502728c0655a72
Christopher Nabors <PillarDD@Gmail.com>
2013-11-28 03:24:34 -0500

    Fixed up the hover functionality and styles. Went ahead and fixed the space problems with happened with my comments.

--- data/js/sane_base_util.js
+++ data/js/sane_base_util.js
@@ -1,28 +1,28 @@
 /*****************************************************************************\
- * sane_base_util.js														 *
+ * sane_base_util.js							     *
  * ------------------------------------------------------------------------- *
  * This file contains the JavaScript code for the base landing page.         *
- *																			 *
- *																			 *
- * FILE CONTENTS															 *
+ *								       	     *
+ *									     *
+ * FILE CONTENTS							     *
  * ------------------------------------------------------------------------- *
- * function main_site_onload()												 *
- *  - Init function that files when the body of the index.html page			 *
- *		is loaded.															 *
- *																			 *
- * function close_div(divName)												 *
- *	- A function that utilizes jQuery to removeClass and addClass to a div.	 *
- *		The divName should have the # included.								 *
- *		Ex. - close_div('#close_me');										 *
- *																			 *
- * function open_div(divName)												 *
- *	- Same function as above, but to open divs.								 *
- *		Ex. - open_div('#open_me');											 *
- *																			 *
- * function Add_Events_To_Project(thumbnail_div)							 *
- *	- A function that adds the mouseenter/mouseleave functions, as well as	 *
- *		the click function that will load the program.						 *
- *		Ex. - Add_Events_To_Project('#site_content_project_one_thumbnail');	 *
+ * function main_site_onload()						     *
+ *  - Init function that files when the body of the index.html page	     *
+ *     is loaded.						     	     *
+ *									     *
+ * function close_div(divName)						     *
+ *	- A function that utilizes jQuery to removeClass and addClass to a   *
+ *	   div. The divName should have the # included.			     *
+ *	   Ex. - close_div('#close_me');				     *
+ *									     *
+ * function open_div(divName)						     *
+ *	- Same function as above, but to open divs.			     *
+ *		Ex. - open_div('#open_me');				     *
+ *									     *
+ * function Add_Events_To_Project(thumbnail_div)			     *
+ *	- A function that adds the mouseenter/mouseleave functions, as well  *
+ *	   as the click function that will load the program.		     *
+ *	   Ex.-Add_Events_To_Project('#site_content_project_one_thumbnail'); *
 \*****************************************************************************/
 function main_site_onload(){
 	// Add events to the project thumbnails.
@@ -40,7 +40,9 @@ function open_div(divName){
 function Add_Events_To_Project(thumbnail_div){
 	// Declare variables
 	var text_placeholder,
-		project_one = "This is project one's placeholder.<br /><br />We can place whatever HTML inside of this variable.<br /><hr class='modified_hr' />Test test.";
+		project_one = "This is project one's placeholder.<br />" +
+			"<br />We can place whatever HTML inside of this " +
+			"variable.<br /><hr class='modified_hr' />Test test.";
 
 	// Determine description content
 	switch(thumbnail_div){
@@ -52,7 +54,7 @@ function Add_Events_To_Project(thumbnail_div){
 	// Enter
 	$(thumbnail_div).mouseenter(function(){
 		close_div('#sane_intro_right_text_default');
-		$('#sane_intro_right_text_new').empty().append(text_placeholder);
+		//$('#sane_intro_right_text_new').empty().append(text_placeholder);
 		open_div('#sane_intro_right_text_new');
 	});
 	// Leave
@@ -61,4 +63,7 @@ function Add_Events_To_Project(thumbnail_div){
 		open_div('#sane_intro_right_text_default');
 	});
 	// Click
+};
+//---------------------------------------------------------------------------\\
+function Initiate_Project(thumbnail_div){
 };
